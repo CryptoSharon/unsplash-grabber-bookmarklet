224fd073e6e8487cc1beb7af1fc4c8051886bdf7
CryptoSharon <35318508+CryptoSharon@users.noreply.github.com>
2020-03-04 22:08:31 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+(() => {
+  const report = text => {
+  	prompt('url & credit', text)
+  }
+
+  const splitAddress = location.href.split('/')
+
+  const ID = splitAddress[splitAddress.length-1]
+
+  const headers = {'Content-Type': 'application/json',
+  'Authorization': 'Client-ID ByNZ31dx-lH97g-4OmS8mwZAltk_x48-RGTbU72B-Lk'}
+
+  fetch('https://api.unsplash.com/photos/' + ID, {headers})
+    .then(r => r.json())
+    .then(json => {
+			//report(JSON.stringify(json.urls))
+      const url = json.urls.raw.concat("&h=800&w=1200")
+      const credit = `Photo by <a href="${json.user.links.html}">${json.user.name}</a> on Unsplash`
+      const html = `<center><img src="${url}"></center><br><center><sup>${credit}</sup></center>`
+			report(html)
+  }).catch(e => report(e))
+})()
